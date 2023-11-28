b97709e775e616b8151629acef9027c1f54930c6
John Caraballo <jcaraballo019@gmail.com>
2023-11-28 12:57:21 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+    <Navbar />
+    <div class="mt-4 p-8">
+        <UCard>
+            <template #header>
+                <h1>What would you like to write about today?</h1>
+            </template>
+            <UInput
+                type="text"
+                placeholder="Best practices writing Nuxt components"
+                v-model="chatInput"
+            />
+        </UCard>
+    </div>
+</template>
+
+<script setup lang="ts">
+const chatInput = ref();
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav className="bg-primary p-4">
+        <NuxtLink to="/">AI Blog</NuxtLink>
+    </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
