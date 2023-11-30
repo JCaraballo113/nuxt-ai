b33716390fca6b0bab1801bf05f329586501a991
John Caraballo <jcaraballo019@gmail.com>
2023-11-30 17:16:38 -0500

    responsive tweaks

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex w-full h-[calc(100vh-96px-64px)]">
-        <div
-            class="w-[40%] border-r border-gray-200 dark:border-gray-800 h-full"
+    <div class="flex w-full h-[calc(100vh-96px-64px)] flex-col md:flex-row">
+        <section
+            class="w-full md:w-[40%] border-r border-gray-200 dark:border-gray-800 md:h-full h-[40%] border-b md:border-b-0"
         >
             <ConversationPanel />
-        </div>
-        <div class="w-[60%] h-full"></div>
+        </section>
+        <section class="w-full md:w-[60%] md:h-full h-[60%]"></section>
     </div>
 </template>
 
